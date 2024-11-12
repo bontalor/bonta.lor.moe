f3ad82a9f644e2e97f9a90d9b71c9b54c37ae577
bontalor <bont@lor.moe>
2024-11-11 19:30:26 -0500

    edit goback.js

--- assets/js/goback.js
+++ assets/js/goback.js
@@ -1,16 +1,34 @@
 function goBack() {
-    // Get the current URL
+    // Get the current URL path
     const currentUrl = window.location.pathname;
 
-    // Split the URL by slashes to work with individual path segments
+    // Split the URL into its segments
     const pathSegments = currentUrl.split('/').filter(segment => segment !== '');
 
-    // Remove the last segment to "go up" one level
+    // Remove the last segment (to go back one directory level)
     pathSegments.pop();
 
-    // Join the remaining segments back into a path
-    const newPath = '/' + pathSegments.join('/');
+    // If the path is empty (i.e., we're at the root), stay there
+    if (pathSegments.length === 0) {
+        window.location.href = '/';
+    } else {
+        // Join the remaining segments to form the new URL path
+        const newPath = '/' + pathSegments.join('/');
 
-    // Redirect the user to the new path
-    window.location.href = newPath || '/';  // If no segments are left, redirect to '/'
+        // Check if the new path exists by attempting to load it
+        fetch(newPath, { method: 'HEAD' })
+            .then(response => {
+                // If the path doesn't exist (404), go back to the root
+                if (response.status === 404) {
+                    window.location.href = '/';
+                } else {
+                    // Otherwise, navigate to the new path
+                    window.location.href = newPath;
+                }
+            })
+            .catch(() => {
+                // If fetch fails, also go to the root
+                window.location.href = '/';
+            });
+    }
 }
